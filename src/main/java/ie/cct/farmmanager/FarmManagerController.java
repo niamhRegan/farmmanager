8ff7cb800701938bc6811088b7fee8532fefd80d
Niamh Regan <niamhrushe@gmail.com>
2020-05-17 16:04:53 +0100

    add endpoint for current-value and current-value-price

--- src/main/java/ie/cct/farmmanager/FarmManagerController.java
+++ src/main/java/ie/cct/farmmanager/FarmManagerController.java
@@ -5,9 +5,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // this annotation tells spring that this is a controller
@@ -15,45 +17,43 @@ import org.springframework.web.bind.annotation.RestController;
 public class FarmManagerController {
 
 	private List<Animal> animals;
-	
+
+	// constructor create empty list of animals
 	public FarmManagerController() {
 		animals = new ArrayList<Animal>();
-		animals.add(new Animal("cow", 300.0f));
-		animals.add(new Animal("pig", 100.0f));
-		animals.add(new Animal("chicken", 0.5f));
 	}
 	
-	// POST is used to receive data from the client and create an entity
+	// CA-1
+	// this annotation is used to tell sprint to connect a HTTP POST to this method and return JSON
+	// it is used to receive data from the client and create an animal
 	// the URL for this method is http://localhost:8080/add-animal
-	// returns the list of animals containing the newly added animal
-	@PostMapping("add-animal")
+	// it creates a new animal and returns the list of animals containing the newly added animal
+	@PostMapping(value = "add-animal", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Animal> addAnimal(@RequestBody Animal animal) {
 		this.animals.add(animal);
 		return animals;
 	}
 
-	// this annotation is used to tell spring to connect a HTTP GET resource to this method
+	// CA-2
+	// this annotation is used to tell spring to connect a HTTP GET resource to this method and return JSON
 	// the URL for this method is http://localhost:8080/average-weight
-	@GetMapping("average-weight")
+	// returns the average weight of each type of animal
+	@GetMapping(value = "average-weight", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Float averageWeight() {
 		Float averageWeight = 0.0f;
 		for (Animal animal: animals) {
 			averageWeight += animal.getWeight();
 		}
 		averageWeight = averageWeight/animals.size();
-
-		// TODO create an object to return JSON instead of Float
 		return averageWeight;
 	}
 
-	@GetMapping("count-by-type")
+	// CA-3
+	// this annotation is used to tell spring to connect a HTTP GET resource to this method and return JSON
+	// the URL for this method is http://localhost:8080/count-by-type
+	// this counts the number of animals of each type
+	@GetMapping(value = "count-by-type", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Map<String, Integer> countByType() {
-		Float minCowWeight = 300.0f;
-		Float minPigWeight = 100.0f;
-		Float minChickenWeight = 5.0f;		
-
-		// TODO check meets minimum weight
-		
 		Map<String, Integer> summary = new HashMap<String, Integer>();
 		for (Animal animal: animals) {
 			if (summary.get(animal.getType()) == null) {
@@ -64,6 +64,49 @@ public class FarmManagerController {
 			}			
 		}
 		return summary;		
+	}
+
+	// CA-4
+	// this annotation is used to tell spring to connect a HTTP GET resource to this method and return JSON
+	// the URL for this method is http://localhost:8080/current-value
+	// returns the current value of full farm stock - only animals that meet min weight requirements
+	@GetMapping(value = "current-value", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Float currentValue() {
+		Float farmValue = 0.0f;
+		for (Animal animal : animals) {
+			switch (animal.getType()) {
+				case "cow":
+					if (animal.getWeight() > 300.0f) {
+						farmValue += 500.0f;
+					}
+					break;
+					
+				case "pig":
+					if (animal.getWeight() > 100.0f) {
+						farmValue += 250.0f;
+					}
+					break;
+
+				case "chicken":
+					if (animal.getWeight() > 0.5f) {
+						farmValue += 5.0f;
+					}
+					break;
+			}
+		}
+		return farmValue;
+	}
+	
+	// CA-5
+	// this annotation is used to tell spring to connect a HTTP GET resource to this method and return JSON
+	// this will map the URL http://localhost:8080/current-value to this code 
+	// you must supply a the current price of all 3 animals cow pig and chicken
+	// e.g http://localhost:8080/current-value-prices?cow=450&pig=220&chicken=2
+	// returns the current value of full farm stock - only animals that meet min weight requirements
+	@GetMapping(value = "current-value-prices", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Float totalValue() {
+		// TODO get price request parameters and use in currentValue method
+		return this.currentValue();
 	}
 	
 }
